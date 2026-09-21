8c3495c162f1d15d9baa4bd4bb9b65c8e09fdedb
Ali Nawaz <abhanbhro052@gmail.com>
2026-09-22 03:18:52 +0500

    chore: update dependencies and add new files
    - Added @vercel/speed-insights to package.json for performance insights.
    - Created robots.txt to allow all user agents.
    - Changed JSX setting in tsconfig.json from 'react-jsx' to 'preserve'.

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,5 +1,31 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  robots: {
+    index: false,
+    follow: false,
+  },
+  alternates: {
+    canonical: '/',
+  },
+  openGraph: {
+    title: 'Acme Dashboard',
+    description: 'The official Next.js Learn Dashboard built with App Router.',
+    url: 'https://next-learn-dashboard.vercel.sh',
+    siteName: 'Acme Dashboard',
+    type: 'website',
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -7,7 +33,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-       <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
